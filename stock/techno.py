adc1ca81eb5ddfeedcf51203c5da28582f24fad1
steak <github@hastake.com>
2013-04-25 23:55:22 +0200

    [enh] add some default techno

--- stock/techno.py
+++ stock/techno.py
@@ -44,8 +44,20 @@ class Techno(models.Model):
         mine1 = T(name="Mine I", extract=1, type="building", cost_prod=10, 
                   cost_ore=1, structural=10, public=1)
 
-        mine2 = T(name="Mine 2", parent=mine1, extract=2, type="building", 
+        mine2 = T(name="Mine II", parent=mine1, extract=2, type="building", 
                   cost_prod=20, cost_ore=1, structural=15, cost_research=100)
+
+        rea1 = T(name="Reactor I", propeller=1, type="component",
+                 cost_prod=2, cost_ore=1, structural=1, public=True)
+
+        shield1 = T(name="Shield I", shield=1, type="component",
+                    cost_prod=5, cost_ore=1, structural=1, public=True)
+
+        cargo1 = T(name="Cargo I", cargo=10, type="component",
+                   cost_prod=2, cost_ore=2, structural=2, parent=rea1)
+
+        scan1 = T(name="Scanner I", syst_scan=1, type="component",
+                  cost_prod=10, cost_ore=5, structural=1, parent=rea1)
 
     @staticmethod
     def get_publics():
