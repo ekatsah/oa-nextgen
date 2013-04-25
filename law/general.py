8f7320fd3b471f8b02728ad9b085430f499b53dd
steak <github@hastake.com>
2013-04-25 09:20:01 +0200

    [enh] give a new player a neutral asset

--- law/general.py
+++ law/general.py
@@ -18,15 +18,26 @@ class Subscribe(Command):
     name = models.CharField(max_length=40);
 
     def resolv(self):
+        neutral = Player.objects.get(name="Void")
+        if neutral.assets.count() == 0:
+            raise Exception("Not enough neutral assets")
+
         player = Player.objects.create(name=self.name, is_active=True, 
                                        is_admin=False)
 
+        # let's give him a solar system
+        new_asset = neutral.assets.all()[0]
+        new_asset.owner = player
+        new_asset.save()
+
+        # also, all the public technology
         for techno in Techno.get_publics():
             player.technos.add(techno)
 
     def __str__(self):
         args = (Command.__str__(self), self.name)
         return "Subscribe: base(%s), name = '%s'" % args
+
 
 class SubscribeView(CreateView):
     model = Subscribe
