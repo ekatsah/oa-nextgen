55f4649f230b3a91ef69b223cd33a1dace7fe62d
steak <github@hastake.com>
2013-04-26 10:55:14 +0200

    [enh] add new field to player model

--- stock/player.py
+++ stock/player.py
@@ -14,9 +14,20 @@ from django.contrib.auth.models import AbstractBaseUser
 
 class Player(AbstractBaseUser):
     name = models.CharField(max_length=40, unique=True, db_index=True)
+    domain = models.CharField(max_length=40, default="Neutral Zone")
+    race = models.CharField(max_length=40, default="mutant")
+    started = models.IntegerField(default=0)
+    alive = models.BooleanField(default=True)
+    budget_tech = models.IntegerField(default=5)
+    budget_spe = models.IntegerField(default=5)
+    budget_cesp = models.IntegerField(default=5)
+    capital_bank = models.IntegerField(default=20000)
+    capital_tech = models.IntegerField(default=0)
+    capital_spe = models.IntegerField(default=0)
+    capital_cesp = models.IntegerField(default=0)
     technos = models.ManyToManyField("Techno", related_name="owners")
     schemes = models.ManyToManyField("Scheme", related_name="possessors")
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    tmp_schemes = models.ManyToManyField("Scheme", related_name="tmpconst")
+    capital = models.ForeignKey("Asset", related_name="capital_of", null=True)
 
     USERNAME_FIELD = 'name'
