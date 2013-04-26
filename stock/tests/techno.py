5268e02c7e196450e5585431603b56376a9972ff
steak <github@hastake.com>
2013-04-26 16:17:29 +0200

    [ref] adapt techno test

--- stock/tests/techno.py
+++ stock/tests/techno.py
@@ -14,36 +14,9 @@ class TechnoTestCase(TestCase):
     def setUp(self):
         pass
 
-    def test_update_feature(self):
+    def test_techno_base(self):
         """update features of techno"""
-        tech = Techno.objects.create(name="TechTestU1")
-        tech.update_feature(public=1, cost_ore=2)
-        self.assertEqual(tech.feature("public"), 1)
-        self.assertEqual(tech.feature("cost_ore"), 2)
-        self.assertEqual(tech.feature("cost_research"), 0)
-
-    def test_update_feature_replace(self):
-        """update features and replace, in techno"""
-        tech = Techno.objects.create(name="TechTestU2")
-        tech.update_feature(public=1, cost_ore=2)
-        self.assertEqual(tech.feature("public"), 1)
-        self.assertEqual(tech.feature("cost_ore"), 2)
-        tech.update_feature(public=3, cost_research=4)
-        self.assertNotEqual(tech.feature("public"), 3)
-        self.assertEqual(tech.feature("cost_research"), 4)
-
-    def test_get_all_features(self):
-        """set features on techno and check all features as a list"""
-        tech = Techno.objects.create(name="TechTestFS")
-        tech.update_feature(YOT=1, SCM=2)
-        for feat in tech.features():
-            self.assertTrue((feat["code"] == "YOT") == (feat["value"] == 1))
-            self.assertTrue((feat["code"] == "SCM") == (feat["value"] == 2))
-            self.assertTrue(feat["code"] == "YOT" or feat["code"] == "SCM")
-
-    def test_get_a_feature(self):
-        """set features on techno and check all features one by one"""
-        tech = Techno.objects.create(name="TechTestAF")
-        tech.update_feature(YOT=1, SCM=2)
-        self.assertEqual(tech.feature("YOT"), 1)
-        self.assertEqual(tech.feature("SCM"), 2)
+        tech = Techno.objects.create(name="TechTestU1", public=1, cost_ore=2)
+        self.assertEqual(tech.public, 1)
+        self.assertEqual(tech.cost_ore, 2)
+        self.assertEqual(tech.cost_research, 0)
